b0dc3c00e8b507580061f96ec812d54e576d3264
TheKrakken11 <clarkwwhalen@gmail.com>
2025-11-15 14:03:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ loader.load(
     const sphereMat = new THREE.MeshStandardMaterial({
       color: 0x0000ff,
       transparent: true,
-      opacity: 1.0,
+      opacity: 0.0,
       emissive: 0x0000ff,
       emissiveIntensity: 0.3,
     });
